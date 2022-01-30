67445ae11b1531ebef721385c21ad02b549bb1cc
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-30 16:14:42 +0200

    done

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -2,12 +2,42 @@ package com.epam.rd.autotasks;
 
 public class CarouselRun {
 
+    public int[] arr;
+    private int k = 0;
+    private int i = 0;
+    private boolean b = false;
+
+    public CarouselRun(int[] arr) {
+        this.arr = arr;
+    }
+
+    public CarouselRun() {
+    }
+
     public int next() {
-       throw new UnsupportedOperationException();
+        if (isFinished() == false) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == 0) {
+                    k++;
+                    i++;
+                    continue;
+                }
+                if (k == arr.length - 1) {
+                    b = true;
+                    return -1;
+                }
+                return arr[i]--;
+            }
+        }
+        return -1;
     }
 
-    public boolean isFinished() {
-        throw new UnsupportedOperationException();
+    public boolean isFinished() {//когда больше нет элементов для уменьшения, возвращает true.
+        // В противном случае возвращает false.
+        if (next() == -1) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,40 @@
 package com.epam.rd.autotasks;
 
-public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
+public class DecrementingCarousel extends CarouselRun {
 
-    }
+        private boolean methodRunIsRun = false;
+        int arrayLength;
+        int[] array;
 
-    public boolean addElement(int element){
-        throw new UnsupportedOperationException();
-    }
+        public DecrementingCarousel(int capacity) {
+            super();
+            array = new int[capacity];
+        }
 
-    public CarouselRun run(){
-       throw new UnsupportedOperationException();
+        public boolean addElement(int element) {
+            int[] array = new int[arrayLength];
+            for (int i = 0; i < array.length; i++) {
+                if (element <= 0) { //Если элемент отрицателен или равен нулю, не добавляет его.
+                    return false;
+                }
+                if (array[i] == array.length - 1) { // Если контейнер заполнен, не добавляет элемент.
+                    return false;
+                }
+                if (DecrementingCarousel.this.methodRunIsRun) {// Если метод run был вызван для создания CarouselRun, не добавляет элемент.
+                    return false;
+                } else {    // Если элемент добавлен успешно, возвращает true. В противном случае – false.
+                    array[i] = element;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public CarouselRun run() {     // создать объект CarouselRun с помощью метода
+            if (this.methodRunIsRun == true) {
+                new CarouselRun(null);
+                return null;
+            }
+            return new CarouselRun(arr);
+        }
     }
-}
